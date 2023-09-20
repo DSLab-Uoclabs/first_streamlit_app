36e1e1de6a6c092ed1f3efb666df190338e44db0
asanchezbelb <143082254+asanchezbelb@users.noreply.github.com>
2023-09-20 08:36:52 +0200

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
-streamlit.title("streamlit is working!!")
+streamlit.title("My mom´s new healthy dinner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
